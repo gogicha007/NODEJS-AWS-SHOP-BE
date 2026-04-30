8fd4a0b03b3b153fe86979073ed42dd09159585c
gogicha007 <gogicha@gmail.com>
2026-04-30 15:34:04 +0400

    feat: add cors validation to headers

--- services/products/lambdas/get-products-list.ts
+++ services/products/lambdas/get-products-list.ts
@@ -1,8 +1,12 @@
 import { products } from "../products-data";
 
 const headers = {
-	"Content-Type": "application/json",
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "https://d2htpstdr8w7tm.cloudfront.net",
+  "Access-Control-Allow-Headers": "Content-Type,Authorization",
+  "Access-Control-Allow-Methods": "GET,OPTIONS",
 };
+
 
 export const handler = async () => {
 	return {
