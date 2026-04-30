119105a6c735d8044397b7ed6e4ab12b4fb18b23
gogicha007 <gogicha@gmail.com>
2026-04-30 17:17:38 +0400

    feat: unit tests for lambda handlers

---
+++ infra/test/products-handlers.test.ts
@@ -0,0 +1,45 @@
+import { describe, expect, it } from "@jest/globals";
+
+import { handler as getProductsById } from "../../services/products/lambdas/get-products-by-id";
+import { handler as getProductsList } from "../../services/products/lambdas/get-products-list";
+import { products } from "../../services/products/products-data";
+
+describe("products lambda handlers", () => {
+	it("returns all products from getProductsList", async () => {
+		const response = await getProductsList();
+
+		expect(response.statusCode).toBe(200);
+		expect(response.headers).toMatchObject({
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "https://d2htpstdr8w7tm.cloudfront.net",
+		});
+		expect(JSON.parse(response.body)).toEqual(products);
+	});
+
+	it("returns a product from getProductsById", async () => {
+		const targetProduct = products[0];
+		const response = await getProductsById({
+			pathParameters: { productId: targetProduct.id },
+		});
+
+		expect(response.statusCode).toBe(200);
+		expect(response.headers).toMatchObject({
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "https://d2htpstdr8w7tm.cloudfront.net",
+		});
+		expect(JSON.parse(response.body)).toEqual(targetProduct);
+	});
+
+	it("returns 404 when getProductsById cannot find a product", async () => {
+		const response = await getProductsById({
+			pathParameters: { productId: "missing-product-id" },
+		});
+
+		expect(response.statusCode).toBe(404);
+		expect(response.headers).toMatchObject({
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "https://d2htpstdr8w7tm.cloudfront.net",
+		});
+		expect(JSON.parse(response.body)).toEqual({ message: "Product not found" });
+	});
+});
