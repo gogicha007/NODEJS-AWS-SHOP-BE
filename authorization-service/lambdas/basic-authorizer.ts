458377b486a88aafcd1a8ba0b08cde6082c3de5a
gogicha007 <gogicha@gmail.com>
2026-05-25 22:35:41 +0400

    feat: return IAM policy in case of success

--- authorization-service/lambdas/basic-authorizer.ts
+++ authorization-service/lambdas/basic-authorizer.ts
@@ -1,64 +1,81 @@
 type Event = {
-    headers?: {
-        authorization?: string
-        Authorization?: string
+  headers?: {
+    authorization?: string;
+    Authorization?: string;
+  };
+  methodArn?: string;
+};
+
+type PolicyEffect = "Allow" | "Deny";
+
+type AuthorizerResponse = {
+  principalId: string;
+  policyDocument: {
+    Version: "2012-10-17";
+    Statement: Array<{
+      Action: "execute-api:Invoke";
+      Effect: PolicyEffect;
+      Resource: string;
+    }>;
+  };
+};
+
+export const handler = async (event: Event): Promise<AuthorizerResponse> => {
+  const headers = event.headers || {};
+  const authHeader = headers["Authorization"] || headers["authorization"];
+
+  const tmp = event.methodArn?.split(":");
+
+  // HTTP 401 in API Gateway
+  if (!authHeader) throw new Error("Unauthorized");
+  if (!tmp || !event.methodArn) throw new Error("Unauthorized");
+
+  const apiGatewayArnTmp = tmp[5].split("/");
+  if (!apiGatewayArnTmp.length) {
+    return generateDeny("me", event.methodArn);
+  }
+
+  const [authType, encoded] = authHeader.split(" ");
+
+  if (authType?.toLowerCase() !== "basic" || !encoded) {
+    return generateDeny("me", event.methodArn);
+  }
+
+  const decoded = Buffer.from(encoded, "base64").toString("utf8");
+  const [username, password] = decoded.split(":");
+
+  if (username && password && password === process.env[username]) {
+    return generateAllow("me", event.methodArn);
+  }
+
+  // HTTP 403 in API Gateway
+  return generateDeny("me", event.methodArn);
+};
+
+const generatePolicy = (
+  principalId: string,
+  effect: PolicyEffect,
+  resource: string,
+) : AuthorizerResponse => {
+  return {
+    principalId: principalId,
+    policyDocument: {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: "execute-api:Invoke",
+          Effect: effect,
+          Resource: resource,
+        },
+      ],
     },
-    methodArn?: string
-}
-
-export const handler = async (event: Event) => {
-    const headers = event.headers || {}
-
-    const authHeader = headers['Authorization'] || headers['authorization']
-
-    const tmp = event.methodArn?.split(':')
-    if (!tmp || !authHeader) throw new Error('Unauthorized')
-
-    const apiGatewayArntmp = tmp[5].split('/')
-    const awsAccountId = tmp[4]
-    const region = tmp[3]
-    const restApiId = apiGatewayArntmp[0]
-    const state = apiGatewayArntmp[1]
-    const method = apiGatewayArntmp[2]
-    let resource = '/'
-    if (apiGatewayArntmp[3]) {
-        resource += apiGatewayArntmp[3]
-    }
-
-    try {
-        const [authType, encoded] = authHeader.split(' ')
-
-        if (authType.toLowerCase() !== 'basic' || !encoded) {
-            return { isAuthorized: false, context: { error: 'Invalid header format' } }
-        }
-
-        const decoded = Buffer.from(encoded, 'base64').toString('utf8')
-        const [username, password] = decoded.split(':')
-
-        if (username && password && password === process.env[username]) return generateAllow('me', event.methodArn)
-    } catch {
-        return {
-            isAuthorized: false, context: { error: 'Access denied' }
-        }
-    }
-
-    return {
-        isAuthorized: false, context: { error: 'Access denied' }
-    }
-}
-
-
-const generatePolicy = (principalId: string, effect: string, resource: string) => {
-    const authResponce = {}
-    Object.assign(authResponce, { principalId: principalId })
-
-    return authResponce
-}
+  };
+};
 
 const generateAllow = (principalId: string, resource: string) => {
-    return generatePolicy(principalId, 'Allow', resource)
-}
+  return generatePolicy(principalId, "Allow", resource);
+};
 
 const generateDeny = (principalId: string, resource: string) => {
-    return generatePolicy(principalId, "Deny", resource)
-}
+  return generatePolicy(principalId, "Deny", resource);
+};
